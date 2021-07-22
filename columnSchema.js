072465ecc82befdcda9a57d222179ff2157ee373
Amit Shrinivas gujar <87461948+amitgujarthingsgosocial@users.noreply.github.com>
2021-07-22 18:18:23 +0530

    Add files via upload

--- columnSchema.js
+++ columnSchema.js
@@ -10,8 +10,13 @@ const colScheama = new Schema({
    sheetId:{
      type:Schema.Types.ObjectId,
      ref:'SheetsCollection'
-   }
-
+   },
+   rows:[
+  
+     {  rawData:{ type: Schema.Types.ObjectId , ref :'RowsCollection'} }
+   
+   ]
+   
 }, { timestamps: true });
 
 module.exports  = model('ColsCollection',colScheama);
